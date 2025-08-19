b022744b9e6ad0540f38b729af291f81666bf7e5
Xu-Shi-Fu <xushifu@bitwormhole.com>
2025-08-19 16:53:12 +0800

    up to v0.9.11

--- module.go
+++ module.go
@@ -2,6 +2,6 @@ package vlog
 
 const (
 	theMoudleName     = "github.com/starter-go/vlog"
-	theMoudleVersion  = "v0.9.10"
-	theMoudleRevision = 100
+	theMoudleVersion  = "v0.9.11"
+	theMoudleRevision = 101
 )
