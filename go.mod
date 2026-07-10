b982897a11ae65688e05122fdeaff2c684705506
kge <kge@bitwormhole.com>
2026-07-10 13:24:01 +0800

    up to v0.10.0

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/starter-go/vlog
 
-go 1.20
+go 1.23
--- module.go
+++ module.go
@@ -2,6 +2,6 @@ package vlog
 
 const (
 	theMoudleName     = "github.com/starter-go/vlog"
-	theMoudleVersion  = "v0.9.18"
-	theMoudleRevision = 108
+	theMoudleVersion  = "v0.10.0"
+	theMoudleRevision = 109
 )
